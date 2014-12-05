6b9d894fc06aaa7c496afcfc2fb776e3b6bec502
Sean Gibat <sgibat@gmail.com>
2014-12-04 20:21:40 -0800

    Cleaned up variable names and organized code a little.
    Just some minor changes -- changed the main callback function from ffff to something more understandable. Declared variables at the top of the callback function. Added a few comments.

--- expander.js
+++ expander.js
@@ -1,27 +1,30 @@
+// Get the 
 var x = document.getElementsByTagName('ul');
 for(var i=0; i<x.length; i++){
   x[i].style.height = "auto";
 }
 
-var ffff = function(){
+var responseHandler = function(){
+  var div, responseHolder, curric;
+
   if (httpRequest.readyState === 4){  
-    var open = true;
-    var div = document.createElement("div");
-    var div2 = document.createElement("div");
-    div2.innerHTML = httpRequest.responseText;
-    var sched = div2.getElementsByClassName('schedule')[0];
-    sched.className += " step";
-    sched.marginTop = "40px";
-    div.appendChild(sched);
-    var curric = document.getElementsByClassName('curriculum')[0];
+    div = document.createElement("div");
+    responseHolder = document.createElement("div");
+    responseHolder.innerHTML = httpRequest.responseText;
+    responseHolder = responseHolder.getElementsByClassName('schedule')[0];
+    responseHolder.className += " step";
+    div.appendChild(responseHolder);
+    curric = document.getElementsByClassName('curriculum')[0];
     div.innerHTML = "<br><br>" + div.innerHTML;
     curric.appendChild(div);
   }
 };
 
+// Testing for the length so that the lesson table of contents
+// won't be appended to the table of contents itself.
 if (window.location.pathname.length > 1){
   var httpRequest = new XMLHttpRequest();
-  httpRequest.onreadystatechange = ffff;
+  httpRequest.onreadystatechange = responseHandler;
   httpRequest.open('GET', 'http://learn.makersquare.com', true);
   httpRequest.send(null);
 }
