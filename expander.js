b3839689fb701fcd53998375f66c5d142dc62ebd
Sean Gibat <sgibat@gmail.com>
2014-12-04 20:28:12 -0800

    Added more comments.
    Realized last commit that I had forgotten to finisht the comments I was writing. Should be pretty understandable now.

--- expander.js
+++ expander.js
@@ -1,12 +1,14 @@
-// Get the 
+// Get the Table of Contents unordered lists and expand them.
 var x = document.getElementsByTagName('ul');
 for(var i=0; i<x.length; i++){
   x[i].style.height = "auto";
 }
 
+// This takes the HTML from the learn app, gets just the "schedule",
+// and inserts it at the bottom of the page. It also adds the "step"
+// class to appear similar to the other boxes on the page.
 var responseHandler = function(){
   var div, responseHolder, curric;
-
   if (httpRequest.readyState === 4){  
     div = document.createElement("div");
     responseHolder = document.createElement("div");
@@ -20,8 +22,9 @@ var responseHandler = function(){
   }
 };
 
-// Testing for the length so that the lesson table of contents
-// won't be appended to the table of contents itself.
+// This makes a get request for the MKS learn app home page.
+// I test for the length of the URL pathname so that the table of contents
+// won't be appended to the table of contents page itself.
 if (window.location.pathname.length > 1){
   var httpRequest = new XMLHttpRequest();
   httpRequest.onreadystatechange = responseHandler;
